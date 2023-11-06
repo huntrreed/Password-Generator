72b7224dcca18bb781a5d0658dd082be9487c000
huntrreed <146265520+huntrreed@users.noreply.github.com>
2023-11-06 15:06:15 -0700

    fixed listening to prompts

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 
@@ -18,28 +17,31 @@ validCharacters += "!#$%^&*()@"
     validCharacters += "1234567890"
   }
 
-  // var PasswordLength = window.prompt("How long do you want your password");
-  // if (numbers) {
-  //   PasswordLength += "i"
-  // }
-
- //code for generating password after prompts are answered
-
-  var characters = "ABCDEFGHIJKLMNOPQURSTUVWXYZabcdefghijklmnopqurstuvwxyz1234567890!@#$%^&*()";
-  var passwordLength = 12;
-  var password = "";
-  for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * characters.length);
-    password += characters.substring(randomNumber, randomNumber + 1);
+  var passwordLength = window.prompt("How long do you want your password?");
+  if (passwordLength) {
+   passwordLength = parseInt(passwordLength); // Convert the input to an integer
+  } else {
+   return "User canceled or entered an invalid password length.";
   }
-  return password;
+
+//code for generating password after prompts are answered
+
+var characters = "abcdefghijklmnopqrstuvwxyz" + validCharacters;
+var passwordLength = passwordLength - 1;
+var password = "";
+for (var i = 0; i <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * characters.length);
+  password += characters.substring(randomNumber, randomNumber + 1);
+}
+return password;
 }
 
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password")
+var password = generatePassword();
+var passwordText = document.querySelector("#password")
 
-  passwordText.value = password;
+passwordText.value = password;
 }
+
 //code for adding password only when generate button is clicked 
 generateBtn.addEventListener("click", writePassword);
