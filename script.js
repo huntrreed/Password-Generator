301e86505d3c3dc7e4868d76767c19ae73dd681f
huntrreed <146265520+huntrreed@users.noreply.github.com>
2023-11-06 12:50:58 -0700

    upload code

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  function writePassword() {
+    var characters = "a!@#$%bcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * characters.length);
+    password += characters.substring(randomNumber, randomNumber +1);
+  }
+  {generateBtn.addEventListener("click", writePassword);
 }
 
+
+
+
+  //var password = generatePassword();
+//var passwordText = document.querySelector("#password")
+
+  //passwordText.value = password;
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
